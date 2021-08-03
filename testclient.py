ee5c01890184de0d26e196d0f6260eb04ecaf103
suulcoder <saulcontreras@Sauls-MacBook-Air.local>
2021-08-02 18:59:31 -0600

    done with message 101

--- testclient.py
+++ testclient.py
@@ -4,6 +4,34 @@ from argparse import ArgumentParser
 
 import slixmpp
 
+class Register(slixmpp.ClientXMPP):
+    def __init__(self, jid, password):
+        slixmpp.ClientXMPP.__init__(self, jid, password)
+
+        self.add_event_handler("session_start", self.start)
+        self.add_event_handler("register", self.register)
+
+    def start(self, event):
+        self.send_presence()
+        self.get_roster()
+        self.disconnect()
+
+    def register(self, iq):
+        iq = self.Iq()
+        iq['type'] = 'set'
+        iq['register']['username'] = self.boundjid.user
+        iq['register']['password'] = self.password
+
+        try:
+            iq.send(now=True)
+            print("New account created", self.boundjid,"\n")
+        except IqError as e:
+            print("Error on registration ", e,"\n")
+            self.disconnect()
+        except IqTimeout:
+            print("Server is not answering\n")
+            self.disconnect()
+
 class SendMsg(slixmpp.ClientXMPP):
     def __init__(self, jid, password, recipient, message):
         slixmpp.ClientXMPP.__init__(self, jid, password)
@@ -14,14 +42,20 @@ class SendMsg(slixmpp.ClientXMPP):
         self.add_event_handler("message", self.message)
 
     async def start(self, event):
+        self.send_presence()
+        await self.get_roster()
         self.send_message(mto=self.recipient,
                           mbody=self.msg,
                           mtype='chat')
 
     def message(self, msg):
-        recipient = input("Write the recipient JID: ") 
-        message = input("Write the message: ")
-        self.send_message(mto=recipient,mbody=message)
+        if msg['type'] in ('chat'):
+            recipient = msg['to']
+            body = msg['body']
+            print(str(recipient) +  ": " + str(body))
+            message = input("Write the message: ")
+            self.send_message(mto=self.recipient,
+                              mbody=message)
 
 
 if __name__ == '__main__':
@@ -62,29 +96,33 @@ if __name__ == '__main__':
 -----------------------------------------------------
              WELCOME THE THE SERVER
 -----------------------------------------------------
-1.  Mostrar todos los usuarios/contactos
-2.  Agregar un usuario a los contactos
+1.  Mostrar todos los usuarios/contactos            
+2.  Agregar un usuario a los contactos              
 3.  Mostrar detalles de un contacto
-4.  Comunicación 1 a 1 
+4.  Comunicación 1 a 1                              !
 5.  Conversaciones grupales
 6.  Definir mensaje de presencia
 7.  Enviar/recibir notificaciones
 8.  Enviar/recibir archivos
-9.  Cerrar sesión con una cuenta
-10. Eliminar la cuenta del servidor
-0.  SALIR
+9.  Eliminar la cuenta del servidor
+0.  Cerrar sesión con una cuenta                    !
 -----------------------------------------------------
             """)
         user = input("Choose your path: ") 
         if(user=="0"):
             control = False
+            print('\nGoodbye! :(\n')
         if(user=="4"):
-            recipient = input("Write the recipient JID: ") 
-            message = input("Write the message: ")
-            xmpp = SendMsg(args.jid, args.password, recipient, message)
-            xmpp.register_plugin('xep_0030') # Service Discovery
-            xmpp.register_plugin('xep_0199') # XMPP Ping
-            xmpp.register_plugin('xep_0045') # Mulit-User Chat (MUC)
-            xmpp.register_plugin('xep_0096') # Jabber Search
-            xmpp.connect()
-            xmpp.process()
+            try:
+                recipient = input("Write the recipient JID: ") 
+                message = input("Write the message: ")
+                xmpp = SendMsg(args.jid, args.password, recipient, message)
+                xmpp.register_plugin('xep_0030') # Service Discovery
+                xmpp.register_plugin('xep_0199') # XMPP Ping
+                xmpp.register_plugin('xep_0045') # Mulit-User Chat (MUC)
+                xmpp.register_plugin('xep_0096') # Jabber Search
+                xmpp.connect()
+                xmpp.process(forever=False)
+            except KeyboardInterrupt as e:
+                print('\nNice chat, dont forget I read all of it haha\n')
+                xmpp.disconnect()
